2530454de3214e562cc6ccab49b882ffc5f946ef
Theophilus Danladi Umar <theophilusumar27@gmail.com>
2025-08-16 11:15:35 +0100

    Add Drug model class
    Introduces the Drug class with fields for id, name, type, category, manufacture date, quantity, and expiry date, along with their getters and setters.

---
+++ src/com/pddPharmacy/data/models/Drug.java
@@ -0,0 +1,71 @@
+package com.pddPharmacy.data.models;
+
+import java.time.LocalDate;
+
+public class Drug {
+    private Long id = 0L;
+    private String name;
+    private Type type;
+    private Category category;
+    private LocalDate manufactureOn;
+    private int quantity;
+    private LocalDate expiryDate;
+
+
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public void setId(Long id){
+        this.id = id;
+    }
+
+    public Long getId(){
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public LocalDate getManufactureOn() {
+        return manufactureOn;
+    }
+
+    public void setManufactureOn(LocalDate manufactureOn) {
+        this.manufactureOn = manufactureOn;
+    }
+
+    public LocalDate getExpiryDate() {
+        return expiryDate;
+    }
+
+    public void setExpiryDate(LocalDate expiryDate) {
+        this.expiryDate = expiryDate;
+    }
+}
