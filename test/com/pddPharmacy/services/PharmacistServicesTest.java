f5a10b2c5d021facbb0d6407626be92d58972364
Theophilus Danladi Umar <theophilusumar27@gmail.com>
2025-08-16 11:17:52 +0100

    Add PharmacistServices unit test skeleton
    Created PharmacistServicesTest.java with initial structure and a placeholder test for adding a pharmacist. No test logic implemented yet.

---
+++ test/com/pddPharmacy/services/PharmacistServicesTest.java
@@ -0,0 +1,22 @@
+package com.pddPharmacy.services;
+
+import com.pddPharmacy.data.models.Drug;
+import com.pddPharmacy.data.repositories.Drugs;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PharmacistServicesTest {
+    PharmacistServices pharmacistServices = new PharmacistServices();
+
+    private Drugs drugs = new Drugs();
+
+    @Test
+    public void test_addPharmacist_PharmacistCountIs1(){
+
+
+
+
+    }
+
+}
