50986c0f97079e9d7638f762341efc00f8ad8d5f
Theophilus Danladi Umar <theophilusumar27@gmail.com>
2025-08-18 14:27:19 +0100

    Refactor PharmacistServicesTest to test addDrug
    Replaces the previous pharmacist addition test with a new test for adding a drug. Sets up AddDrugRequest and verifies that the drug count increases after addition. Also adds missing imports and initializes dependencies in setUp.

--- test/com/pddPharmacy/services/PharmacistServicesTest.java
+++ test/com/pddPharmacy/services/PharmacistServicesTest.java
@@ -1,21 +1,44 @@
 package com.pddPharmacy.services;
 
+import com.pddPharmacy.data.models.Category;
 import com.pddPharmacy.data.models.Drug;
+import com.pddPharmacy.data.models.Type;
 import com.pddPharmacy.data.repositories.Drugs;
+import com.pddPharmacy.dtos.Request.AddDrugRequest;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PharmacistServicesTest {
-    PharmacistServices pharmacistServices = new PharmacistServices();
+    PharmacistServices pharmacistServices;
+    Drugs drugs;
 
-    private Drugs drugs = new Drugs();
+    @BeforeEach
+    void setUp() {
+        pharmacistServices = new PharmacistServices();
+        drugs = new Drugs();
+    }
+
 
     @Test
-    public void test_addPharmacist_PharmacistCountIs1(){
+    public void test_addDrug_DrugCountIs1(){
+    assertEquals(0L, drugs.getsize());
+    AddDrugRequest addDrugRequest = new AddDrugRequest();
+
+    addDrugRequest.setName("panadol");
+    addDrugRequest.setType(Type.SYRUP);
+    addDrugRequest.setManufactureOn(LocalDate.of(2025,10,11));
+    addDrugRequest.setQuantity(1);
+    addDrugRequest.setCategory(Category.ANALGESTIC);
+    addDrugRequest.setExpiryDate(addDrugRequest.getManufactureOn().plusMonths(10));
 
 
-
+    pharmacistServices.addDrug(addDrugRequest);
+    assertEquals(1L, drugs.getsize());
 
     }
 
