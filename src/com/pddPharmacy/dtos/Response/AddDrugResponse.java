e5e0094719870a426114037b8c3813b0927a07eb
Theophilus Danladi Umar <theophilusumar27@gmail.com>
2025-08-18 14:27:39 +0100

    Add fields to AddDrugResponse DTO
    Introduced 'drugs' and 'drugName' fields with corresponding getters and setters to the AddDrugResponse class for improved response data handling.

--- src/com/pddPharmacy/dtos/Response/AddDrugResponse.java
+++ src/com/pddPharmacy/dtos/Response/AddDrugResponse.java
@@ -1,6 +1,22 @@
 package com.pddPharmacy.dtos.Response;
 
 public class AddDrugResponse {
+    private int drugs;
+    private String drugName;
 
+    public int getDrugs() {
+        return drugs;
+    }
 
+    public void setDrugs(int drugs) {
+        this.drugs = drugs;
+    }
+
+    public String getDrugName() {
+        return drugName;
+    }
+
+    public void setDrugName(String drugName) {
+        this.drugName = drugName;
+    }
 }
