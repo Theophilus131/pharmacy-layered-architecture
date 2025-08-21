c802ec64c9969dd5b18eb8a705e027f5a3d0fa1c
Theophilus Danladi Umar <theophilusumar27@gmail.com>
2025-08-21 09:35:45 +0100

    Add PharmacistController for managing pharmacist operations
    Introduced `PharmacistController` in the `com.pddPharmacy.controllers` package to handle pharmacist-related actions, including recording new drugs, buying drugs, and retrieving available drug details.

---
+++ src/com/pddPharmacy/controllers/PharmacistController.java
@@ -0,0 +1,28 @@
+package com.pddPharmacy.controllers;
+
+import com.pddPharmacy.dtos.Request.AddDrugRequest;
+import com.pddPharmacy.dtos.Request.BuyDrugsRequest;
+import com.pddPharmacy.dtos.Response.AddDrugResponse;
+import com.pddPharmacy.dtos.Response.AvailableDrugResponse;
+import com.pddPharmacy.services.PharmacistServices;
+
+import java.util.List;
+
+public class PharmacistController {
+    private PharmacistServices pharmacistServices = new PharmacistServices();
+
+    public AddDrugResponse recordNewDrug(AddDrugRequest addDrugRequest){
+        return pharmacistServices.addDrug(addDrugRequest);
+    }
+
+    public String buyDrug(BuyDrugsRequest buyDrugsRequest){
+
+        pharmacistServices.buyDrugs(buyDrugsRequest);
+        return "Drugs bought successfully";
+    }
+
+    public List<AvailableDrugResponse> getAvailableDrugs(){
+
+        return pharmacistServices.getAvaibleDrug();
+    }
+}
