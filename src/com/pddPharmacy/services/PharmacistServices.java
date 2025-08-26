c577cfca88db6a8a93f4cbc813b1dc23435d5db2
Theophilus Danladi Umar <theophilusumar27@gmail.com>
2025-08-26 15:09:17 +0100

    Add new drug purchase and retrieval features and introduce constructor injection in `PharmacistServices`
    Includes:
    - Added `buyDrugs` method for purchasing drugs.
    - Added `getAvaibleDrug` method for retrieving all available drugs.
    - Refactored `PharmacistServices` to use constructor injection, improving testability.
    - Utilized `Validator` and `Mappers` utilities for request validation and mapping.

--- src/com/pddPharmacy/services/PharmacistServices.java
+++ src/com/pddPharmacy/services/PharmacistServices.java
@@ -3,28 +3,59 @@ package com.pddPharmacy.services;
 import com.pddPharmacy.data.models.Drug;
 import com.pddPharmacy.data.repositories.Drugs;
 import com.pddPharmacy.dtos.Request.AddDrugRequest;
+import com.pddPharmacy.dtos.Request.BuyDrugsRequest;
 import com.pddPharmacy.dtos.Response.AddDrugResponse;
+import com.pddPharmacy.dtos.Response.AvailableDrugResponse;
+import com.pddPharmacy.exceptions.InvalidDrugQualityException;
+import java.util.ArrayList;
+import java.util.List;
+
 
 import static com.pddPharmacy.utils.Mappers.map;
+import static com.pddPharmacy.utils.Validator.validate;
 
-public class PharmacistServices {
-   private Drugs drugs = new Drugs();
 
-    public PharmacistServices() {
-       this.drugs = new Drugs();
+
+    public class PharmacistServices {
+    private final Drugs drugs;
+
+    public PharmacistServices(Drugs drugs) {
+       this.drugs = drugs;
     }
+
 
     public AddDrugResponse addDrug(AddDrugRequest addDrugRequest){
         Drug drug=new Drug();
 
+        validate(addDrugRequest);
         map(addDrugRequest, new Drug());
         drugs.save(drug);
         return map(drug);
-
-
     }
 
 
+    public void buyDrugs(BuyDrugsRequest buyDrugsRequest) {
+        Drug drug = drugs.findByName(buyDrugsRequest.getDrugName());
+        drug.setQuantity(drug.getQuantity() - buyDrugsRequest.getQuantity());
+        drugs.save(drug);
+    }
+
+    public List<AvailableDrugResponse> getAvaibleDrug() {
+        List<Drug> allDrugs = drugs.findAll();
+        List<AvailableDrugResponse> availableDrugs = new ArrayList<>();
+
+        for(Drug drug: allDrugs){
+            if(drug.getQuantity() > 0){
+                AvailableDrugResponse availableDrugResponse;
+                availableDrugResponse = new AvailableDrugResponse();
+
+                availableDrugResponse.setName(drug.getName());
+                availableDrugResponse.setQuantity(drug.getQuantity());
+                availableDrugResponse.setIsExpired(drug.getExpiryDate());
+            }
+        }
+        return availableDrugs;
+    }
 
 }
 
